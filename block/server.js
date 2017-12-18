fc180314ac48333d7946c82f4f2aced5ca3183b9
pilgaard <emil.pilgaard@gmail.com>
2017-12-18 12:26:59 +0100

    .

--- block/server.js
+++ block/server.js
@@ -1,13 +1,11 @@
 'use strict';
 
 const express = require('express');
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
 const app = express();
+
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+// App
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
